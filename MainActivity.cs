064e38b911c6160029c8a5059179b6aa6e65218c
karolki <karolki337@interia.pl>
2018-09-13 20:55:39 +0200

    Init

---
+++ MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using TaxiMobile;
+using Acr.UserDialogs;
+
+namespace TaxiMobile.Droid
+{
+    [Activity(Label = "TaxiMobile", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            UserDialogs.Init(this);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new TaxiMobile.App());
+        }
+
+    }
+}
+
